467c5af974b64c88d869648ba666c3b7a9424743
tnargy <clsscmstng@gmail.com>
2024-11-07 21:09:16 +0000

    changed client to a nc equiv trying to fix zig to read bigger buffers

--- echo/client.c
+++ echo/client.c
@@ -9,7 +9,6 @@ int main(int argc, char const* argv[])
 {
     int status, valread, client_fd;
     struct sockaddr_in serv_addr;
-    char* hello = "Hello from client";
     char buffer[1024] = { 0 };
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
@@ -35,7 +34,10 @@ int main(int argc, char const* argv[])
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(client_fd, hello, strlen(hello), 0);
+    char msg[BUFSIZ];
+    while (fgets(msg, BUFSIZ, stdin) != NULL)
+    
+    send(client_fd, msg, strlen(msg), 0);
     printf("Hello message sent\n");
     valread = read(client_fd, buffer,
                    1024 - 1); // subtract 1 for the null
--- primetime/src/main.zig
+++ primetime/src/main.zig
@@ -11,7 +11,8 @@ pub fn main() !void {
     try posix.bind(lsnr, &addr.any, addr.getOsSockLen());
     try posix.listen(lsnr, 128);
 
-    var buf: [1024]u8 = undefined;
+    var buf: [1 << 10]u8 = undefined;
+    std.debug.print("Server started...\n", .{});
     while (true) {
         var client_address: net.Address = undefined;
         var client_address_len: posix.socklen_t = @sizeOf(net.Address);
@@ -24,16 +25,14 @@ pub fn main() !void {
 
         std.debug.print("{} connected\n", .{client_address});
 
-        const timeout = posix.timeval{ .tv_sec = 5, .tv_usec = 0 };
+        const timeout = posix.timeval{ .sec = 5, .usec = 0 };
         try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.RCVTIMEO, &std.mem.toBytes(timeout));
         try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.SNDTIMEO, &std.mem.toBytes(timeout));
 
         const stream = std.net.Stream{ .handle = socket };
+        const read = try stream.readAll(&buf);
+        if (read == 0) break;
 
-        const read = try stream.read(&buf);
-        if (read == 0) {
-            continue;
-        }
-        try stream.writeAll(buf[0..read]);
+        _ = try stream.write(buf[0..read]);
     }
 }
