4814dc2c3335c1838b48ea2f5d8db34f73350322
tnargy <clsscmstng@gmail.com>
2024-12-21 10:36:44 -0600

    Update rpg.py
    Remove notes

--- rpg/rpg.py
+++ rpg/rpg.py
@@ -90,11 +90,3 @@ class Character:
         self.TensionCheck()
         if not self.EncounterCheck():
             self.EventCheck()
-
-
-onyx = Character()
-onyx.lightsource = 5
-onyx.tension = 6
-onyx.exhaustion = 2
-print(f"Path: I, II, III, 2x3 room," \
-      f" IV, 3x3 room(Flesh Eater)")
