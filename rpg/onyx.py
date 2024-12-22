22a7e6ec6eb5df2e3974653c9ed9252777d6f822
tnargy <clsscmstng@gmail.com>
2024-12-21 23:40:03 -0600

    Defeated Flesh Eater

--- rpg/onyx.py
+++ rpg/onyx.py
@@ -4,5 +4,7 @@ onyx = Character()
 onyx.lightsource = 5
 onyx.tension = 6
 onyx.exhaustion = 2
-print(f"Path: I, II, III, 2x3 room," \
-      f" IV, 3x3 room(Flesh Eater)")
+
+# Path: I, II, III, 2x3 room, IV, 3x3 room*
+
+# Flesh Eater: Killed Dropped Speed Potion
