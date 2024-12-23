1d6da684c17249fe0dca3fc8de957b09cf827bb8
onyx <onyx@archlinux>
2024-12-22 22:19:27 -0600

    onyx

---
+++ rpg/onyx.py
@@ -0,0 +1,10 @@
+from rpg import Character
+
+onyx = Character()
+onyx.lightsource = 5
+onyx.tension = 6
+onyx.exhaustion = 2
+
+# Game Notes
+# Path: I, II, III, 2x3 room, IV, 3x3 room*
+# Flesh Eater: Killed Dropped Speed Potion
